76a4d5312217e00060b471d825afd8dd689db468
Md. Rayhan Shorker <raihanshorker56@gmail.com>
2024-11-22 23:50:03 +0600

    implement create order logic

--- src/app/confiq/modules/car/car.services.ts
+++ src/app/confiq/modules/car/car.services.ts
@@ -2,9 +2,9 @@ import { Tcar } from "./car.interface";
 import { CarModel } from "./car.model";
 
 const createCarIntoDB = async (carData: Tcar) => {
-  const student = new CarModel(carData); //create an instance
+  const car = new CarModel(carData); //create an instance
 
-  const result = await student.save(); //built in instance method
+  const result = await car.save(); //built in instance method
   return result;
 };
 const getAllCarFromDB = async()=>{
--- src/app/confiq/modules/order/order.service.ts
+++ src/app/confiq/modules/order/order.service.ts
@@ -1,11 +1,26 @@
 
+import { CarModel } from "../car/car.model";
 import { Torder } from "./order.interface";
 import { OrderModel } from "./order.model";
 
 const createOrderIntoDB = async (orderData: Torder) => {
-    const student = new OrderModel(orderData);
-
-    const result = await student.save(); 
+    const order = new OrderModel(orderData);
+    const car = await CarModel.findById(orderData.car)
+   
+    if (!car) {
+        throw new Error("Car not found.");
+      }
+      if (car.quantity < orderData.quantity) {
+        throw new Error('Insufficient Stock')
+       }
+      car.quantity -= orderData.quantity;
+   
+    if ( car.quantity=== 0) {
+      car.inStock = false;
+    }
+   
+    await car.save();
+    const result = await order.save(); 
     return result;
   };
 
